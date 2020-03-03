1674a681eae8d0db325ab0e0061020d2e07b7e19
jbschool <jbowen10@msudenver.edu>
2020-03-02 21:21:36 -0700

    stub for request handler testing

--- hpotter_api/test/__main__.py
+++ hpotter_api/test/__main__.py
@@ -6,11 +6,15 @@ from os import chdir
 moduleDir = dirname(abspath(__file__)) + '/'
 chdir(moduleDir)
 
-import hpotter_api.test.test_schema as test_schema
 from hpotter_api.test.createDb import create_database
+import hpotter_api.test.test_schema as test_schema
+import hpotter_api.test.test_server as test_server
 
 if __name__ == '__main__':
     create_database()
     suite = unittest.TestLoader().loadTestsFromModule(test_schema)
+    unittest.TextTestRunner().run(suite)
+
+    suite = unittest.TestLoader().loadTestsFromModule(test_server)
     unittest.TextTestRunner().run(suite)
     
